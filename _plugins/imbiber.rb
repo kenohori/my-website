82fe0d2a9c10eb1e5d1f68bf8176f9efd46f8d28
Ken Arroyo Ohori <g.a.k.arroyoohori@tudelft.nl>
2016-12-07 10:26:24 -0600

    Search function, updated imbiber

--- _plugins/imbiber.rb
+++ _plugins/imbiber.rb
@@ -304,10 +304,10 @@ class Imbiber
 			:lang => :en,
 			:nameformat => :firstlast,
 			:titlecase => :sentence,
-			:beforeentry => "<p>",
-			:afterentry => "</p>",
+			:beforeentry => "<div class=\"filteredelement\">",
+			:afterentry => "</div>",
 			:beforegrouptitle => "<h3>",
-			:aftergrouptitle => "</h3>",
+			:aftergrouptitle => "<span id=\"filteredcount\"></span></h3>",
 			:beforesubgrouptitle => "<h4>",
 			:aftersubgrouptitle => "</h4>"
 		}
@@ -975,7 +975,7 @@ class Imbiber
 		sorted_groups.each do |group|
 			# pp group[1][:entries]
 			if idswithprefix != false then
-				html << "<section id=\"" << idswithprefix << group[0] << "\">\n"
+				html << "<section id=\"" << idswithprefix << group[0] << "\" class=\"filteredgroup\">\n"
 			end
 			html << @options[:beforegrouptitle]
 			if group[1].has_key?(:nicename) then
@@ -993,18 +993,24 @@ class Imbiber
 				# pp entry
 				if groupby == :classthenyear then
 					if entry[:sortingvalue][0..3] != last_year then
-						if last_year == "" then
+						if last_year != "" then
 							html << "</section>\n"
 						end
-						html << "<section id=\"" << idswithprefix.to_s << group[0] << entry[:sortingvalue][0..3] << "\">\n"
+						html << "<section id=\"" << idswithprefix.to_s << group[0] << entry[:sortingvalue][0..3] << "\" class=\"filteredgroup\">\n"
 						html << "<h4>" << entry[:sortingvalue][0..3] << "</h4>"
 						last_year = entry[:sortingvalue][0..3]
 					end
 				end
 				html << entry[:entry] << "\n"
 			end
-			if @idswithprefix != false then
+
+			if groupby == :year and @idswithprefix != false then
 				html << "</section>\n"
+			else
+				html << "</section>\n"
+				if @idswithprefix != false then
+					html << "</section>\n"
+				end
 			end
 		end
 
